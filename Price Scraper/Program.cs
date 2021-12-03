675551824fe380d6f36939d4d64a52a44aa235c3
IceLord420 <78491489+IceLord420@users.noreply.github.com>
2021-12-03 13:00:43 +0100

    Price-Scraper

---
+++ Price Scraper/Program.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Price_Scraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.emag.bg/igri-konzola-kompiutyr/c?ref=hp_menu_quick-nav_3096_10&type=category";
+            string selector = "//div[@class='card-item card-standard js-product-data']/div/div/div[3]/a";
+            Console.OutputEncoding = Encoding.UTF8;
+
+            GenerateCSV(url, selector);
+
+            string[] urls = File.ReadAllLines("./links.csv");
+
+            Scraper.ScrapePrices(urls);
+
+        }
+
+
+        public static void GenerateCSV(string mainUrl, string linkSelector)
+        {
+            Console.WriteLine("Creating csv file");
+
+            if (File.Exists("./links.csv"))
+            {
+                Console.WriteLine("CSV already created. Skipping.");
+                return;
+            }
+
+            var web = new HtmlWeb();
+            var doc = web.Load(mainUrl);
+
+            var links = doc.DocumentNode.SelectNodes(linkSelector)
+                .Select(node => node.Attributes["href"].Value);
+
+            var output = string.Join(Environment.NewLine, links);
+
+            File.WriteAllText("./links.csv", output);
+            Console.WriteLine("csv file created.");
+        }
+    }
+}
+
---
+++ Price Scraper/Scraper.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Price_Scraper
+{
+    class Scraper
+    {
+        public static void ScrapePrices(string[] urls)
+        {
+            for (int j = 0; j < urls.Length; j++)
+            {
+                Thread t = new Thread(new ParameterizedThreadStart(ScraperTask));
+                t.Start(urls[j]);
+            }
+
+            Thread.Sleep(1000);
+        }
+
+        private static void ScraperTask(object value)
+        {
+            string url = (string)value;
+
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var name = doc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div/section[1]/div/div[2]/div/div[1]/h1").InnerText;
+            var price = doc.DocumentNode.SelectSingleNode("/html/body/div[3]/div[2]/div/section[1]/div/div[2]/div/div[2]/div[2]/div/div/div[2]/form/div[1]/div[1]/div/div/p[2]").InnerText;
+
+            Console.WriteLine("| {0} | is with Price: {1}", name.Trim(), price.Trim());
+        }
+    }
+}
